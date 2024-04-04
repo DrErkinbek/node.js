f1eee4bc2d5a974d00fc4bb720dd4b0ff72f25e8
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2024-04-04 12:22:30 -0500

    8th commit

--- express/app.js
+++ express/app.js
@@ -1,40 +1,18 @@
-const http = require('http');
-const { readFileSync } = require('fs');
+const express = require('express');
+const path = require('path');
+const app = express();
 
-// get all files
-const homePage = readFileSync('./navbar-app/index.html');
-const homeStyles = readFileSync('./navbar-app/styles.css');
-const homeImage = readFileSync('./navbar-app/logo.svg');
-const homeLogic = readFileSync('./navbar-app/browser-app.js');
+// setup static and middleware
+app.use(express.static('./public'))
 
-const server = http.createServer((req, res) => {
-	const url = req.url;
-	
-	if (url === '/') {
-		res.writeHead(200, { 'content-type': 'text/html' });
-		res.write(homePage)
-		res.end();
-	} else if (url === '/about') {
-		res.writeHead(200, { 'content-type': 'text/html' });
-		res.write('<h2>This is about page');
-	} else if (url === '/styles.css') {
-		res.writeHead(200, { 'content-type': 'text/css' });
-		res.write(homeStyles);
-		res.end()
-	} else if (url === '/logo.svg') {
-		res.writeHead(200, { 'content-type': 'image/svg+xml' });
-		res.write(homeImage);
-		res.end()
-	} else if (url === '/browser-app.js') {
-		res.writeHead(200, { 'content-type': 'text/javascript' });
-		res.write(homeLogic)
-		res.end()
-	} else {
-		res.writeHead(404, { 'content-type': 'text/html' });
-		res.write('<h2>404</h2>')
-		res.end();
-	}
-	
-});
+app.get('/', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './navbar-app/index.html'));
+})
 
-server.listen(5500);
+app.all('*', (req, res) => {
+	res.status(404).send('resource not found');
+})
+
+app.listen(7000, () => {
+	console.log('Server is listening on port 7000');
+})
