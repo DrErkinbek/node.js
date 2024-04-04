176d2216a9b0701cae7cb698345f9957788d106a
Erkinbek <erkinbekabdurakhmanov@gmail.com>
2024-04-04 11:55:55 -0500

    7th commit

--- express/app.js
+++ express/app.js
@@ -3,10 +3,13 @@ const { readFileSync } = require('fs');
 
 // get all files
 const homePage = readFileSync('./navbar-app/index.html');
-
+const homeStyles = readFileSync('./navbar-app/styles.css');
+const homeImage = readFileSync('./navbar-app/logo.svg');
+const homeLogic = readFileSync('./navbar-app/browser-app.js');
 
 const server = http.createServer((req, res) => {
 	const url = req.url;
+	
 	if (url === '/') {
 		res.writeHead(200, { 'content-type': 'text/html' });
 		res.write(homePage)
@@ -14,6 +17,18 @@ const server = http.createServer((req, res) => {
 	} else if (url === '/about') {
 		res.writeHead(200, { 'content-type': 'text/html' });
 		res.write('<h2>This is about page');
+	} else if (url === '/styles.css') {
+		res.writeHead(200, { 'content-type': 'text/css' });
+		res.write(homeStyles);
+		res.end()
+	} else if (url === '/logo.svg') {
+		res.writeHead(200, { 'content-type': 'image/svg+xml' });
+		res.write(homeImage);
+		res.end()
+	} else if (url === '/browser-app.js') {
+		res.writeHead(200, { 'content-type': 'text/javascript' });
+		res.write(homeLogic)
+		res.end()
 	} else {
 		res.writeHead(404, { 'content-type': 'text/html' });
 		res.write('<h2>404</h2>')
